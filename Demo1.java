148a28f3adfe4630dbfbd1f75bb6c216f0fec0c1
Srakesh44 <65937950+Srakesh44@users.noreply.github.com>
2024-03-24 01:36:08 +0530

    Initial commit

---
+++ Demo1.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Demo1{
+    public static void main(String[] args) {
+        
+        System.out.println("Hello from java program:)");
+        // System.out.println(new Date());
+        Object dateCreated = new Date();
+        String datecreation = dateCreated.toString();
+        System.out.println("Date: " + datecreation);
+    }
+}
